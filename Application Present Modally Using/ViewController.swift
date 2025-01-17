aadc6a166f4b68743a491c6b8c31195525e9002d
zemfirazeynal <zemfirazeynal@gmail.com>
2025-01-17 11:25:13 -0800

    Initial Commit

---
+++ Application Present Modally Using/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Application Present Modally Using
+//
+//  Created by Zemfira Asadzade on 17.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Application Present Modally UsingUITests/Application_Present_Modally_UsingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Application_Present_Modally_UsingUITestsLaunchTests.swift
+//  Application Present Modally UsingUITests
+//
+//  Created by Zemfira Asadzade on 17.01.25.
+//
+
+import XCTest
+
+final class Application_Present_Modally_UsingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
